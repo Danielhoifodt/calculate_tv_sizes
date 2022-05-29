06e63b2dc741d33b9c9bef4ad9616d3a2ab28cc9
dh_netsense.no <dh@netsense.no>
2022-05-29 19:22:52 +0200

    number

--- utregning.js
+++ utregning.js
@@ -2,26 +2,34 @@
 
 function calculate()
 {
+	//Get value from the inputs
 	let large = document.getElementById("large").value;
 	let small = document.getElementById("small").value;
 
+	//Use the function below to insert the variables large and small
 	let area_large = convertToArea(large)
 	let area_small = convertToArea(small)
 
+	//Get the percent of difference between large area and small area and round to whole numbers
 	let percent = Math.round(((area_large - area_small)/area_large)*100)
 
+	//Output text with the variables into the result html tag.
 	document.getElementById("result").innerHTML = " Den store skjermen er " + percent + " % større enn den miste skjermen. Og de er henholdsvis " + area_large + " cm^2 og " + area_small + " cm^2.";
 }
 
 
 function convertToArea(number)
 {
+	//Find the diagonal in cm by multiplying 2.54
 	let cm_diagonal =  number*2.54
 
-	let cm_horisontal = cm_diagonal * (9/16)
+	//Find the height of the tv by mulitplying the diagonal by 0.49
+	let cm_horisontal = cm_diagonal * 0.49
 
+	//Use the formula for straigh angle trinagles a^2 = b^2 + c^2 to find the last side
 	let katet = Math.sqrt((cm_diagonal*cm_diagonal) - (cm_horisontal*cm_horisontal))
-
+	
+	//Return the area in cm^2
 	return katet*cm_diagonal
 
 }
